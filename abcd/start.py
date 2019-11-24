bc1926b9749fb90b3781843809f84756ea76d1e3
bishnupada81 <53900688+bishnupada81@users.noreply.github.com>
2019-11-24 07:44:20 -0800

    Add files via upload

--- abcd/start.py
+++ abcd/start.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Bishnupada@localhost:3306/abcd'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost:3306/abcd'
 db = SQLAlchemy(app)
 
 class User(db.Model):
